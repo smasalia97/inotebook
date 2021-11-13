32cf1230457f7f097c1047dd4213edb7ed5b6953
Saiyam Masalia <masaliasaiyam97@gmail.com>
2021-11-13 14:02:51 +0530

    First Commit

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Saiyamisagoodb$oy";
+
+const fetchuser = (req, res, next) => {
+  // Get thr user from the jwt toke and  add the id to req object
+  const token = req.header("auth-token");
+  if (!token) {
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
+  }
+  
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .send({ error: "Please authenticate using a valid token" });
+  }
+};
+
+module.exports = fetchuser;
--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./Components/About";
+import Alert from "./Components/Alert";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Navbar from "./Components/Navbar";
+import Signup from "./Components/Signup";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert alert={alert} />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home showAlert={showAlert} />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+              <Route exact path="/login">
+                <Login showAlert={showAlert} />
+              </Route>
+              <Route exact path="/signup">
+                <Signup showAlert={showAlert} />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
